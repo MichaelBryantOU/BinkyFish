19d78671afc45ef5c1c7c24afc911a12bff90160
Nathan Feist <nf603315@ohio.edu>
2019-11-15 10:24:10 -0500

    Enemy Follow Script

---
+++ BinkyFish/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Speed = 5;
+
+    private Transform Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Player.position) > .6)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, Speed * Time.deltaTime);
+        }
+    }
+}
---
+++ BinkyFish/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 2;
+
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 Movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(Movement * Speed);
+
+    }
+}
